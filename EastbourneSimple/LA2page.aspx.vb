8070fecdd725220236b7167e67d56e4e897c7093
uklsped <david.spendley@bsuh.nhs.uk>
2017-12-06 13:49:16 +0000

    Correct EndofDayElf

--- EastbourneSimple/LA2page.aspx.vb
+++ EastbourneSimple/LA2page.aspx.vb
@@ -1213,15 +1213,15 @@ Partial Public Class LA2page
                         'mrepcontrol.RemoteLockElf()
                         If Breakdown Then
                             'This means there are still open faults
+                             If Caller = "EndDay" Then
+                            WriteRecovery()
+                            Else
                             mrepcontrol.RemoteLockElf(False)
+                            End If
                         Else
                             If lastState = "Fault" Then
                                 'This means there were open faults but they have been closed so need to close them off.
-                                   If Caller = "EndDay" Then
-                            WriteRecovery()
-                        Else
-                            mrepcontrol.RemoteLockElf(False)
-                        End If
+                                mrepcontrol.WriteFaultIDTable()
                             End If
                             DavesCode.Reuse.WriteAuxTables(EquipmentID, Logoffuser, Comment, 102, 5, Breakdown, suspendnull, repairstatenull, False)
                         End If
