4761f82b45a6ae1ac09ab6893fce2a52ae206768
David Spendley <david.spendley@nhs.net>
2019-01-08 17:05:42 +0000

    Fix letter count problem

--- EastbourneSimple/controls/CommentBoxuc.ascx
+++ EastbourneSimple/controls/CommentBoxuc.ascx
@@ -1,21 +1,31 @@
 ﻿<%@ Control Language="VB" AutoEventWireup="false" CodeFile="CommentBoxuc.ascx.vb" Inherits="controls_CommentBoxuc" %>
 <script type="text/javascript">
-    function Count() {
-                 var i = document.getElementById("CommentBox").value.length;
+  <%-- function Count() {
+        alert("What the fuck")
+        var txt = document.getElementById('<%=TextBox.ClientID%>');
+                 var i = txt.value.length;
         if (i < 251) {
-            document.getElementById("CommentWordCount").innerHTML = 250 - i;
+            document.getElementById('<%=CommentWordCount.ClientID%>').innerHTML = 250 - i;
             
         }
         else 
-            document.getElementById("CommentWordCount").innerHTML = "You have exceeded the max text please delete some characters";
+            document.getElementById('<%=CommentWordCount.ClientID%>').innerHTML = "You have exceeded the max text please delete some characters";
            
         
         return;
-    }
-    
+    }--%>
+   
+
 </script>
-<asp:TextBox ID="CommentBox" runat="server" TextMode="Multiline"  onkeyup="Count()" ClientIDMode="Static"  ViewStateMode="Disabled"></asp:TextBox>
+
+<asp:TextBox ID="TextBox" runat="server" TextMode="Multiline"    ViewStateMode="Disabled"></asp:TextBox>
+<asp:RequiredFieldValidator ID="CommentValidation" ControlToValidate="TextBox"  runat="server" Display="Dynamic"></asp:RequiredFieldValidator>
                        <br/>
 <asp:Literal ID="Literal1" runat="server">Characters Remaining: </asp:Literal>
 
-<asp:Label ID="CommentWordCount" runat="server" Text="" ClientIDMode="Static"></asp:Label>
+<asp:Label ID="CommentWordCount" runat="server" Text="" ></asp:Label>
+
+
+
+
+
--- EastbourneSimple/controls/CommentBoxuc.ascx.vb
+++ EastbourneSimple/controls/CommentBoxuc.ascx.vb
@@ -2,37 +2,121 @@
 Partial Class controls_CommentBoxuc
     Inherits UserControl
     Public Property Currentcomment() As String
-    'Public Property LinacName() As String
     Public Property BoxChanged() As String
+    'Public Property ValidateGroup As String
+    'Public Property ErrorMessage As String
     Const MAXCHARCOUNT = 250
 
+    Protected Sub Page_PreRender(sender As Object, e As EventArgs) Handles Me.PreRender
+        'Dim csname2 As String = "ButtonClickScript"
+        'Dim cstype As Type = Me.GetType()
+        ''Get a ClientScriptManager reference from the Page class.
+        'Dim cs As ClientScriptManager = Page.ClientScript
+        'If Not cs.IsClientScriptBlockRegistered(cstype, csname2) Then
+
+        '    Dim cstext2 As StringBuilder = New StringBuilder()
+        '    cstext2.Append("function Count() {alert(""Fuck Off""); return} ")
+        '    'cstext2.Append("<script type=\"text/javascript\"> function DoClick() {")
+        '    'cstext2.Append(" </")
+        '    'cstext2.Append("script>")
+        '    cs.RegisterClientScriptBlock(cstype, csname2, cstext2.ToString(), True)
+        'End If
+    End Sub
 
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-        CommentBox.Width = Unit.Pixel(355)
-        CommentBox.Height = Unit.Pixel(120)
+        TextBox.Width = Unit.Pixel(355)
+        TextBox.Height = Unit.Pixel(120)
+        'Dim csname2 As String = "ButtonClickScript"
+        'Dim cstype As Type = Me.GetType()
+        ''Get a ClientScriptManager reference from the Page class.
+        'Dim cs As ClientScriptManager = Page.ClientScript
+        'If Not cs.IsClientScriptBlockRegistered(cstype, csname2) Then
+
+        '    Dim cstext2 As StringBuilder = New StringBuilder()
+        '    cstext2.Append("function Count() {Alert(""Fuck Off""}; Return ")
+        '    'cstext2.Append("<script type=\"text/javascript\"> function DoClick() {")
+        '    'cstext2.Append(" </")
+        '    'cstext2.Append("script>")
+        '    cs.RegisterClientScriptBlock(cstype, csname2, cstext2.ToString(), True)
+        'End If
+
+
+
+
+        ' CommentWordCount.Text = TextBox.MaxLength.ToString()
+
+        ' TextBox.Attributes.Add("onKeyUp",
+        '"javascript:document.getElementById('" + CommentWordCount.ClientID +
+        '"').setAttribute('value', (" + TextBox.MaxLength +
+        '" - parseInt(document.getElementById('" + TextBox.ClientID +
+        '"').getAttribute('value').length)));")
+
+
+
+
+        'Dim cs As ClientScriptManager
+        'cs = Page.ClientScript
+        'Dim csname2 As String = "JSCR"
+        'Dim cstype As Type = Me.GetType()
+        'Dim cstext2 As StringBuilder = New StringBuilder()
+        'Dim strScript As String = "<script>"
+        'strScript += "function Count() {}"
+        'strScript += "</script>"
+        'cstext2.Append("<script type=\'text/javascript\'> function Count() {} ")
+        ''cstext2.Append("Form1.Message.value='Text from client script.'} </")
+        'cstext2.Append("/script>")
+        'cs.RegisterClientScriptBlock(cstype, csname2, strScript.ToString(), True)
+
+        'ClientScriptManager.clientscript
+        'ScriptManager.RegisterStartupScript(Page, Me.GetType(), "JSCR", "Function Count()", True)
+        'Dim scriptString As String = "<script type='text/javascript'>Function Count(){Alert('What the fuck')}</script>"
+        'Page.ClientScript.RegisterClientScriptBlock(Me.GetType(), "ClientScriptFunction", scriptString)
+        'TextBox.Attributes.Add("onKeyUp", scriptString10
+        TextBox.Attributes.Add("onKeyUp", String.Format("Count(""{0}"", ""{1}"")", TextBox.ClientID, CommentWordCount.ClientID))
+
+        Dim csname2 As String = "ButtonClickScript"
+        Dim cstype As Type = Me.GetType()
+        'Get a ClientScriptManager reference from the Page class.
+        Dim cs As ClientScriptManager = Page.ClientScript
+        If Not cs.IsClientScriptBlockRegistered(cstype, csname2) Then
+
+            Dim cstext2 As StringBuilder = New StringBuilder()
+            cstext2.Append("function Count(TextID, CountID) {")
+            cstext2.Append("var txt = document.getElementById(TextID);")
+            cstext2.Append("var i = txt.value.length;")
+            cstext2.Append("if (i < 251); {document.getElementById(CountID).innerHTML = 250 - i;}")
+            'cstext2.Append("Else document.getElementById(""<%=CommentWordCount.ClientID%>').innerHTML = ""You have exceeded the max text please delete some characters"";)")
+            cstext2.Append("return;}")
+            cs.RegisterClientScriptBlock(cstype, csname2, cstext2.ToString(), True)
+        End If
         If Not IsPostBack Then
 
-            If (Not HttpContext.Current.Application(BoxChanged) Is Nothing) Then
+            If Not HttpContext.Current.Application(BoxChanged) Is Nothing Then
                 Currentcomment = HttpContext.Current.Application(BoxChanged).ToString
-                CommentBox.Text = Currentcomment
+                TextBox.Text = Currentcomment
 
                 'CommentWordCount.Text = MAXCHARCOUNT - Currentcomment.Length
             End If
         End If
-        Currentcomment = CommentBox.Text
+        Currentcomment = TextBox.Text
         CommentWordCount.Text = MAXCHARCOUNT - Currentcomment.Length
     End Sub
 
-    Protected Sub CommentBox_TextChanged(sender As Object, e As EventArgs) Handles CommentBox.TextChanged
-        Currentcomment = CommentBox.Text
+    Protected Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles TextBox.TextChanged
+        Currentcomment = TextBox.Text
         Application(BoxChanged) = Currentcomment
         'DavesCode.Reuse.ReturnApplicationState(BoxChanged)
     End Sub
     Public Sub ResetCommentBox()
         Currentcomment = String.Empty
         Application(BoxChanged) = Nothing
-        CommentBox.Text = Currentcomment
+        TextBox.Text = Currentcomment
         CommentWordCount.Text = MAXCHARCOUNT - Currentcomment.Length
 
     End Sub
+    Public Sub SetValidation(ByVal ValidateGroup As String, ByVal ErrorMessage As String)
+        CommentValidation.ValidationGroup = ValidateGroup
+        CommentValidation.ErrorMessage = ErrorMessage
+    End Sub
+
 End Class
